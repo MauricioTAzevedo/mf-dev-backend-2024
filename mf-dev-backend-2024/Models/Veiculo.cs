edae89e08bc0425983b5a9a4d0c5abecd32d8c73
Maurício Thales <164433651+MauricioTAzevedo@users.noreply.github.com>
2024-10-09 21:24:58 -0300

    Mudanças

--- mf-dev-backend-2024/Models/Veiculo.cs
+++ mf-dev-backend-2024/Models/Veiculo.cs
@@ -17,9 +17,11 @@ namespace mf_dev_backend_2024.Models
         public string Placa { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar o ano de fabricação!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar o Ano do Modelo!")]
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
 
 
